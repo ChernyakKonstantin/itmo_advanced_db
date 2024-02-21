5bc37773888fc076bcf6bc0113e04991dc5c7354
Konstantin Chernyak <chernyakonstantin@gmail.com>
2024-02-21 11:00:23 +0300

    Limit clickhouse aggragation to wait same day packets only

--- clickhouse_aggregator/aggregator.py
+++ clickhouse_aggregator/aggregator.py
@@ -1,31 +1,9 @@
 import logging
 import os
-
+import datetime
 import clickhouse_connect
 
 logging.basicConfig(level=logging.INFO)
-
-sql_command_mark_existing_data_as_old = (
-    "INSERT INTO TABLE sensor_storage.sensors_data_per_second (sensor_id, timestamp, measurement, n_samples, sign) "
-    "SELECT sensor_id, timestamp, measurement, n_samples, -1 "
-    "FROM sensor_storage.sensors_data_per_second"
-)
-sql_command_update_with_new_data = (
-    "INSERT INTO TABLE sensor_storage.sensors_data_per_second (sensor_id, timestamp, measurement, n_samples) "
-    "WITH rounded_ts_data AS ( "
-    "SELECT sensor_id, toDateTime(timestamp) as timestamp, measurement "
-    "FROM sensor_storage.sensors_data "
-    "), "
-    "aggregated_data AS ( "
-    "SELECT sensor_id, timestamp, AVG(measurement) as measurement, COUNT(*) AS n_samples "
-    "FROM rounded_ts_data "
-    "GROUP BY sensor_id, timestamp "
-    ") "
-    "SELECT * FROM aggregated_data;"
-)
-sql_command_optimize = (
-    "OPTIMIZE TABLE sensor_storage.sensors_data_per_second_local ON CLUSTER sensor_data_cluster_2S_2R"
-)
 
 
 if __name__ == "__main__":
@@ -35,6 +13,32 @@ if __name__ == "__main__":
         password=os.environ["CLICKHOUSE_PASSWORD"] if "CLICKHOUSE_PASSWORD" in os.environ else "12345",
         database=os.environ["CLICKHOUSE_DATABASE"] if "CLICKHOUSE_DATABASE" in os.environ else "sensor_storage",
     )
+    today_start = datetime.datetime.now().date().strftime("%F %T.%f")
+
+    sql_command_mark_existing_data_as_old = (
+        f"INSERT INTO TABLE sensor_storage.sensors_data_per_second (sensor_id, timestamp, measurement, n_samples, sign) "
+        f"SELECT sensor_id, timestamp, measurement, n_samples, -1 "
+        f"FROM sensor_storage.sensors_data_per_second "
+        f"WHERE timestamp >= {today_start}"
+    )
+    sql_command_update_with_new_data = (
+        f"INSERT INTO TABLE sensor_storage.sensors_data_per_second (sensor_id, timestamp, measurement, n_samples) "
+        f"WITH rounded_ts_data AS ( "
+        f"   SELECT sensor_id, toDateTime(timestamp) as timestamp, measurement "
+        f"   FROM sensor_storage.sensors_data "
+        f"   WHERE timestamp >= {today_start}"
+        f"), "
+        f"aggregated_data AS ( "
+        f"   SELECT sensor_id, timestamp, AVG(measurement) as measurement, COUNT(*) AS n_samples "
+        f"   FROM rounded_ts_data "
+        f"   GROUP BY sensor_id, timestamp "
+        f") "
+        f"SELECT * FROM aggregated_data;"
+    )
+    sql_command_optimize = (
+        "OPTIMIZE TABLE sensor_storage.sensors_data_per_second_local ON CLUSTER sensor_data_cluster_2S_2R"
+    )
+
     logging.info("Marking data as depreciated")
     summary = client.command(sql_command_mark_existing_data_as_old)
 
